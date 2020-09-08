db27cb07007a514533efcf5c93589cf9f0b9876a
Jason R <99057964@mydavinci.nl>
2020-09-08 13:15:59 +0200

    Tafel van 6 af en getest nu bezig met tafel van 3

--- Lab opdrachten /lab 3/tafel van 3/assets/lab3.js
+++ Lab opdrachten /lab 3/tafel van 3/assets/lab3.js
@@ -3,4 +3,13 @@
 //Da Vinci College. Software Development.
 
 //Code voor de tafel van 6.
+var tafel6 = 6;//Alle getallen worden vermedigvulgd met 6.
+for(var i=0; i <= 10; i++){//For loop om steeds 1 getal bij te voegen als het kleiner is dan 10.
+    var antwoord = tafel6*i;
+document.write(tafel6,'x',i,'=', antwoord,'<br/>');//Hier worden de berekeningen met antwoord getoond op het scherm.
+};
+
+
+
+
 
