cab7fca36c9a7d8a0820c5446e55c9b0de94a63f
Jason R <99057964@mydavinci.nl>
2020-09-08 14:31:34 +0200

    Laatste stap ook af van lab 3 en getest dus lab 3 is helemaal af.

--- Lab opdrachten /lab 3/berekeningen /assets /lab3.js
+++ Lab opdrachten /lab 3/berekeningen /assets /lab3.js
@@ -3,8 +3,11 @@
 //Da Vinci College. Software Development.
 
 //3. Berekeningen uitvoeren.
-var getallen = [12,6,10,5,12];//Alle getallen in een array voor de berekening.
-var uitkomst = 12+6*10/5-12;//De uitkomst voor de berekening.
-document.write(12,'+',6,'x',10,'/',5,'-',12,'=', uitkomst);
+var uitkomst1 = 12+6*10/5-12;//De uitkomst voor de berekening.
+document.write(12,'+',6,'x',10,'/',5,'-',12,'=', uitkomst1,'<br/>');//Laat de berekening zien op het scherm.
 
 //4. Berekeningen uitvoeren met invoer van getal.
+var invoer = prompt('Type een getal in.');//Promot voor de gebruiker zodat diegene een getal kan invoeren.
+var uitkomst2 = invoer + 12+4*5/9-13;
+document.write(invoer,'+',12,'+',4,'x',5,'/',9,'-',13,'=', uitkomst1);//Laat de berekening zien op het scherm.
+//Het is my opgevallen dat het antwoord hetzelfde is.
