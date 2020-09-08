e202cab1ef62ae59a229b1f65fcd68168f4995fb
Jason R <99057964@mydavinci.nl>
2020-09-08 13:51:57 +0200

    Berekening af van lab 3 ben nu bij de laatste stap van lab 3.

---
+++ Lab opdrachten /lab 3/berekeningen /assets /lab3.js
@@ -0,0 +1,10 @@
+//Jason Ronoastro
+//Lab 3. 08-09-2020.
+//Da Vinci College. Software Development.
+
+//3. Berekeningen uitvoeren.
+var getallen = [12,6,10,5,12];//Alle getallen in een array voor de berekening.
+var uitkomst = 12+6*10/5-12;//De uitkomst voor de berekening.
+document.write(12,'+',6,'x',10,'/',5,'-',12,'=', uitkomst);
+
+//4. Berekeningen uitvoeren met invoer van getal.
